7157a857c24b0a750380284c7b64bdab8d2d1344
VAZVictor <victor.ehrenmueller.jensen@gmail.com>
2026-08-02 18:38:27 +0200

    vercel analytics

--- clicker/frontend/src/app/app.ts
+++ clicker/frontend/src/app/app.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 @Component({
   selector: 'app-root',
