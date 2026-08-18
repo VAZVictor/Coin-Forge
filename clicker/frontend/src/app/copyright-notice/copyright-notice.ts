96c7b7221913fa53ae0f3b9dd081506c24f48ca6
VAZVictor <victor.ehrenmueller.jensen@gmail.com>
2026-08-18 16:42:37 +0200

    copyright-notice component added

---
+++ clicker/frontend/src/app/copyright-notice/copyright-notice.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-copyright-notice',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './copyright-notice.html',
+  styleUrl: './copyright-notice.css'
+})
+export class CopyrightNotice {
+  /** Shows just the © line when true; adds the disclaimer line when false. */
+  readonly compact = input<boolean>(false);
+
+  protected readonly year = new Date().getFullYear();
+}
